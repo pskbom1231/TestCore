78f9ee73ae46378e7fe4e4d8a37cb7216eb35fb3
Passakorn Panphromthumrong <passakorn.bom@gmail.com>
2016-05-27 16:49:19 +0700

    aaaa

--- src/TestCore/Controllers/HomeController.cs
+++ src/TestCore/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace TestCore.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
+            
             return View();
         }
 
